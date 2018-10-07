5a8adea8ed8dc79d6cd256c92121fded4d826805
Sergio Andres Ortiz Pinto <s.andrespinto@gmail.com>
2018-10-07 17:06:32 -0500

    Summary

--- js/my-app.js
+++ js/my-app.js
@@ -554,6 +554,29 @@ function feedback_validate(val) {
 
 
 
+});
+
+myApp.onPageInit('registro2', function (page){
+    
+    		(function() {
+				var formWrap = document.getElementById( 'fs-form-wrap' );
+
+				[].slice.call( document.querySelectorAll( 'select.cs-select' ) ).forEach( function(el) {	
+					new SelectFx( el, {
+						stickyPlaceholder: false,
+						onChange: function(val){
+							document.querySelector('span.cs-placeholder').style.backgroundColor = val;
+						}
+					});
+				} );
+
+				new FForm( formWrap, {
+					onReview : function() {
+						classie.add( document.body, 'overview' ); // for demo purposes only
+					}
+				} );
+			})();
+    
 })
 
 
