2f115e92c0973fe2c48961aced4fdc6efd8fd5fd
Carolina Betancur <caromoreno1626@hotmail.com>
2018-11-06 23:22:52 -0500

    Summary

--- js/my-app.js
+++ js/my-app.js
@@ -664,6 +664,25 @@ myApp.onPageInit('questions', function(page){
 
 myApp.onPageInit('registro2', function (page){
 
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.autocomplete');
+//     var instances = M.Autocomplete.init(elems, options);
+//   });
+
+
+//   // Or with jQuery
+
+//   $(document).ready(function(){
+//     $('input.autocomplete').autocomplete({
+//       data: {
+//         "Apple": null,
+//         "Microsoft": null,
+//         "Google": 'https://placehold.it/250x250'
+//       },
+//     });
+//   });
+  
 	// var calendarDefault = myApp.calendar({
 	//     input: '#birthday',
 	// }); 
